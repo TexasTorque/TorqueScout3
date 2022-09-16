49e90d239ab93c968b279ed8a301c29a5db018aa
Juicestus <jus@gtsbr.org>
2022-09-15 19:18:49 -0500

    Removed unused files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import { default as Home } from "./pages/Home";
@@ -23,4 +22,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-serviceWorker.unregister();
+// serviceWorker.unregister();
