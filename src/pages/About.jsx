2924d27d57aa3a854de1918eccba30ea964047b1
Juicestus <jus@gtsbr.org>
2022-09-17 23:14:48 -0500

    About improvments

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -17,8 +17,17 @@ const About = () => {
     <div className="home">
       <div className="container mt-4">
         <Group name="About">
-          <p className="mt-4" style={{width: '18rem'}}>Torque Scout 3 was initially written by <a href="https://justusl.com">Justus Languell</a> in 1 day.</p>
-          <ButtonFull name="View the code" callback={() => navigate('https://github.com/texastorque/TorqueScout3')} />
+          <p className="mt-4" style={{width: '18rem'}}>Torque Scout 3 is proprietary software built by the software team at <a href="https://texastorque.org">Texas Torque</a>.</p>
+          <p className="mt-2" style={{width: '18rem'}}>Torque Scout 3 was initially written by <a href="https://justusl.com">Justus Languell</a> in 1 day.</p>
+          <h5>Authors</h5>
+          <p className="mt-2" style={{width: '18rem'}}>
+            <ul>
+              <li>Justus Languell</li>
+              <li>Suhas Guddeti</li>
+              <li>Jack Pittenger</li>
+            </ul>
+          </p>
+          {/* <ButtonFull name="View the code" callback={() => window.location = 'https://github.com/texastorque/TorqueScout3'} /> */}
           <ButtonFull name="Back to home" callback={() => navigate('/')} />
         </Group>
       </div>
